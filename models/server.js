1dc0f7a04bc0b2044f600b7840dd297809c7aeae
Maribel <jazmin2003castillon@hotmail.com>
2024-06-05 14:27:01 -0500

    Primer Commit

---
+++ models/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require('cors');
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.usuariosPath='/api/usuarios';
+    //Middelware
+    this.middlewares();
+    //Rutas de la aplicacion
+    this.routes();
+  }
+  //se define el metodo middleware que publicara la carpeta public
+  middlewares(){
+    //cors
+    this.app.use(cors());
+    //lectura y parseo
+    this.app.use(express.json());
+    //directorio publico
+    this.app.use(express.static('public'));
+  }
+  routes() {
+    this.app.use(this.usuariosPath, require('../routes/user'))
+  }
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor corriendo como puerto: ", this.port);
+    });
+  }
+}
+module.exports = Server;
